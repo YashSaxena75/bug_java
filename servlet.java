9f275e469eafd27bb5cd5e4b8b92edf0d09534c8
Yash <yashsaxena986@gmail.com>
2023-03-30 10:21:15 +0530

    Fix XSS

--- servlet.java
+++ servlet.java
@@ -18,11 +18,11 @@ public class EchoServlet extends HttpServlet {
  
       // Write the response message, in an HTML page
       try {
-         StringEscapeUtils.escapeHtml("<!DOCTYPE html>");
-         StringEscapeUtils.escapeHtml("<html><head>");
-         StringEscapeUtils.escapeHtml("<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>");
-         StringEscapeUtils.escapeHtml("<title>Echo Servlet</title></head>");
-         StringEscapeUtils.escapeHtml("<body><h2>You have enter</h2>");
+         out.println("<!DOCTYPE html>");
+         out.println("<html><head>");
+         out.println("<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>");
+         out.println("<title>Echo Servlet</title></head>");
+         out.println("<body><h2>You have enter</h2>");
  
          // Retrieve the value of the query parameter "username" (from text field)
          String username = request.getParameter("username");
@@ -30,41 +30,41 @@ public class EchoServlet extends HttpServlet {
          // Get empty string or string of white spaces if user did not fill in
          if (username == null
                || (username = htmlFilter(username.trim())).length() == 0) {
-            StringEscapeUtils.escapeHtml("<p>Name: MISSING</p>");
+            out.println("<p>Name: MISSING</p>");
          } else {
-            StringEscapeUtils.escapeHtml("<p>Name: " + username + "</p>");
+            out.println("<p>Name: " + username + "</p>");
          }
  
          // Retrieve the value of the query parameter "password" (from password field)
          String password = request.getParameter("password");
          if (password == null
                || (password = htmlFilter(password.trim())).length() == 0) {
-            StringEscapeUtils.escapeHtml("<p>Password: MISSING</p>");
+            out.println("<p>Password: MISSING</p>");
          } else {
-            StringEscapeUtils.escapeHtml("<p>Password: " + password + "</p>");
+            out.println("<p>Password: " + password + "</p>");
          }
  
          // Retrieve the value of the query parameter "gender" (from radio button)
          String gender = request.getParameter("gender");
          // Get null if the parameter is missing from query string.
          if (gender == null) {
-            StringEscapeUtils.escapeHtml("<p>Gender: MISSING</p>");
+            out.println("<p>Gender: MISSING</p>");
          } else if (gender.equals("m")) {
-            StringEscapeUtils.escapeHtml("<p>Gender: male</p>");
+            out.println("<p>Gender: male</p>");
          } else {
-            StringEscapeUtils.escapeHtml("<p>Gender: female</p>");
+            out.println("<p>Gender: female</p>");
          }
  
          // Retrieve the value of the query parameter "age" (from pull-down menu)
          String age = request.getParameter("age");
          if (age == null) {
-            StringEscapeUtils.escapeHtml("<p>Age: MISSING</p>");
+            out.println("<p>Age: MISSING</p>");
          } else if (age.equals("1")) {
-            StringEscapeUtils.escapeHtml("<p>Age: &lt; 1 year old</p>");
+            out.println("<p>Age: &lt; 1 year old</p>");
          } else if (age.equals("99")) {
-            StringEscapeUtils.escapeHtml("<p>Age: 1 to 99 years old</p>");
+            out.println("<p>Age: 1 to 99 years old</p>");
          } else {
-            StringEscapeUtils.escapeHtml("<p>Age: &gt; 99 years old</p>");
+            out.println("<p>Age: &gt; 99 years old</p>");
          }
  
          // Retrieve the value of the query parameter "language" (from checkboxes).
@@ -73,19 +73,19 @@ public class EchoServlet extends HttpServlet {
          String[] languages = request.getParameterValues("language");
          // Get null if the parameter is missing from query string.
          if (languages == null || languages.length == 0) {
-            StringEscapeUtils.escapeHtml("<p>Languages: NONE</p>");
+            out.println("<p>Languages: NONE</p>");
          } else {
-            StringEscapeUtils.escapeHtml("<p>Languages: ");
+            out.println("<p>Languages: ");
             for (String language : languages) {
                if (language.equals("c")) {
-                  StringEscapeUtils.escapeHtml("C/C++ ");
+                  out.println("C/C++ ");
                } else if (language.equals("cs")) {
-                  StringEscapeUtils.escapeHtml("C# ");
+                  out.println("C# ");
                } else if (language.equals("java")) {
-                  StringEscapeUtils.escapeHtml("Java ");
+                  out.println("Java ");
                }
             }
-            StringEscapeUtils.escapeHtml("</p>");
+            out.println("</p>");
          }
  
          // Retrieve the value of the query parameter "instruction" (from text area)
@@ -94,30 +94,30 @@ public class EchoServlet extends HttpServlet {
          if (instruction == null
               || (instruction = htmlFilter(instruction.trim())).length() == 0
               || instruction.equals("Enter your instruction here...")) {
-            StringEscapeUtils.escapeHtml("<p>Instruction: NONE</p>");
+            out.println("<p>Instruction: NONE</p>");
          } else {
-            StringEscapeUtils.escapeHtml("<p>Instruction: " + instruction + "</p>");
+            out.println("<p>Instruction: " + instruction + "</p>");
          }
  
          // Retrieve the value of the query parameter "secret" (from hidden field)
          String secret = request.getParameter("secret");
-         StringEscapeUtils.escapeHtml("<p>Secret: " + secret + "</p>");
+         out.println("<p>Secret: " + secret + "</p>");
  
          // Get all the names of request parameters
          Enumeration names = request.getParameterNames();
-         StringEscapeUtils.escapeHtml("<p>Request Parameter Names are: ");
+         out.println("<p>Request Parameter Names are: ");
          if (names.hasMoreElements()) {
             out.print(htmlFilter(names.nextElement().toString()));
          }
          do {
             out.print(", " + htmlFilter(names.nextElement().toString()));
          } while (names.hasMoreElements());
-         StringEscapeUtils.escapeHtml(".</p>");
+         out.println(".</p>");
  
          // Hyperlink "BACK" to input page
-         StringEscapeUtils.escapeHtml("<a href='form_input.html'>BACK</a>");
+         out.println("<a href='form_input.html'>BACK</a>");
  
-         StringEscapeUtils.escapeHtml("</body></html>");
+         out.println("</body></html>");
       } finally {
          out.close();  // Always close the output writer
       }
