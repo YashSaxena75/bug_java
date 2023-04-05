7910253b937046da9ea307f3b56c0a5a21c07b82
Yash Saxena <35332582+YashSaxena75@users.noreply.github.com>
2023-04-05 16:44:07 +0530

    Update Buff.cpp

--- Buff.cpp
+++ Buff.cpp
@@ -1,25 +1,9 @@
 // Online C++ compiler to run C++ program online
-#include <cstdio> #include <cstring> #include <iostream>
-2
-3
-4
-5
-6
-7 int main()
-8 ~ {
-9
-10
-11
-12
-13
-14
-15
-16
-17 *
-18
-19
-20
-21
+#include <cstdio> 
+#include <cstring> 
+#include <iostream>
+int main()
+{
 char input [255];
 char password [4];
 std:: cout << "Enter password:
@@ -31,15 +15,8 @@ if (* (input+i)==0)
 break;
 else
 *(password+i)=*(input+1);
-22
-23
-24
-25
-26
-27
-28
-29 }
-printf("%dIn", i);
+}
+printf("%dI", i);
 printf ("%s\n", input); printf ("%s \n" ,password);
 return 0;
 }
